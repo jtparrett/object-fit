9a95b9275e2e08b9f52087d41edff0070df913d5
Josh Parrett <jtparrett@me.com>
2016-08-19 16:45:31 +0100

    re-factor

--- object-fit.js
+++ object-fit.js
@@ -1,11 +1,12 @@
 (function(window, $){
-  $.fn.objectFit = function(opts){
-    var options = $.extend({
-          force: false,
-          position: 'center',
-          type: 'cover'
-        }, opts);
+  var defaults = {
+        force: false,
+        position: 'center',
+        type: 'cover'
+      };
 
+  $.fn.objectFit = function(opts){
+    var options = $.extend(defaults, opts);
     if(document.body.style.objectFit != undefined && !options.force){
       return false;
     }
@@ -18,19 +19,17 @@
 
 var objectFit = function(el, options){
   var el = $(el);
-  this.opts = $.extend({
-    src: el.attr('src'),
-    parent: el.parent()
-  }, options);
-  
+  this.opts = options;
+  this.src = el.attr('src');
+  this.parent = el.parent();
   this.setBackground();
   el.remove();
 };
 
 objectFit.prototype.setBackground = function(){
-  this.opts.parent.css({
+  this.parent.css({
     'background-position': this.opts.position,
     'background-size': this.opts.type,
-    'background-image': 'url(' + this.opts.src + ')'
+    'background-image': 'url(' + this.src + ')'
   });
 };
